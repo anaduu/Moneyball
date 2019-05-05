f4737c8b74cd2e1a0d651aa7ba674f3e499855cb
Lovro Sindičić <lovro.sindicic@gmail.com>
2019-05-05 14:34:26 +0200

    greska u kodu

--- data/distribucija.R
+++ data/distribucija.R
@@ -1,5 +1,5 @@
 df1 <- read.csv('croatia.csv')
-v1 <- df[["Overall"]]
+v1 <- df1[["Overall"]]
 df2 <- read.csv('brazil.csv')
-v2 <- df[["Overall"]]
+v2 <- df2[["Overall"]]
 t.test(v1,v2,var.equal=T,alternative="less")
