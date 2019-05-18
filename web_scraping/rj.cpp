1323b032069b2b4dc53eae096703cb5977214beb
Lovro Sindičić <lovro.sindicic@gmail.com>
2019-05-18 22:32:33 +0200

    izracunavam prosjecni overall po zemlji u test setu

---
+++ web_scraping/rj.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <map>
+#include <set>
+#include <algorithm>
+using namespace std;
+int broj=0;
+map <string,double> mapa1;
+map <string,double> mapa2;
+set <string> s;
+int main()
+{
+	ifstream myfile;
+	myfile.open("dr.csv");
+
+	while(myfile.good())
+	{
+		string line;
+		string line1;
+
+		getline(myfile, line, ',');
+    		// cout << "Nat: " << line << " " ; 
+		s.insert(line);
+		mapa1[line]++;
+    		getline(myfile, line1 );
+   		int b = atoi(line1.c_str());
+		//  printf("broj je %d\n",b);
+		mapa2[line]+=b; 
+   	}
+
+	myfile.close();
+set <string>::iterator si;
+printf("U setu se nalazi:\n");
+double rezultat;
+int imam_ukupno=0;
+for(si=s.begin();si!=s.end();++si)
+{
+ 	if(mapa1[*si]>20) {
+				rezultat=mapa2[*si]/mapa1[*si];
+				cout << *si <<" "<<mapa1[*si]<<" " <<mapa2[*si];
+				printf(" %lf\n",rezultat);
+				++imam_ukupno;
+			  }
+}
+//printf("Duljina seta je: %d\n",s.size());
+printf("Na kraju imam ukupno %d država\n",imam_ukupno);
+return 0;
+}
+
