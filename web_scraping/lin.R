994438197a25c79d9e4e4f565c686c17f850ee8d
Lovro Sindičić <lovro.sindicic@gmail.com>
2019-05-20 12:51:32 +0200

    R kod drzave

---
+++ web_scraping/lin.R
@@ -0,0 +1,8 @@
+df1 <- read.csv('dr1.csv')
+v1 <- df1[["overall"]]
+v2 <- df1[["koef"]]
+lm(v1~v2)
+df <- data.frame(v1,v2)
+plot(v2~v1,df)
+model <- lm(v2~v1,df)
+lines(df$v1,predict(model),col='blue')
