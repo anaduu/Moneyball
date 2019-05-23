704cba372f3ce3950c7b6d03fcfc6b32808d6d92
Sindicic Lovro <silovro@pr2-18.studenti.math.hr>
2019-05-23 13:58:59 +0200

    koef u c++

--- igraci_web_scraping/rj.cpp
+++ igraci_web_scraping/rj.cpp
@@ -9,10 +9,10 @@
 using namespace std;
 struct par
 {
-	string ime;
-	int broj;
+	int uk;
+	double koef;
 };
-int broj=0;
+int broj=0,b,c,a;
 map <string,double> mapa1;
 map <string,double> mapa2;
 set <string> s;
@@ -23,66 +23,46 @@ par par;
 int main()
 {
 	ifstream myfile;
-	myfile.open("vrijednost_overall.csv");
+	myfile.open("vratari-tezina-visina.csv");
 
 	while(myfile.good())
 	{
-		string line;
 		string line1;
+		string line2;
+		string line3;
 
-		getline(myfile, line, ',');
-    		// cout << "Nat: " << line << " " ; 
-	
-		s.insert(line);
-		mapa1[line]++;
-    		getline(myfile, line1 );
-   		int b = atoi(line1.c_str());
-		//  printf("broj je %d\n",b);
-		par.ime=line;
-		par.broj=b;
+		getline(myfile, line1, ',');
+    		a=atoi(line1.c_str());	
+		getline(myfile, line2,',');
+		b=atoi(line2.c_str());
+    		getline(myfile, line3 );
+   		
+		//Funkcija koja ince pretvara u cm
+		string  s1=line3.substr(0,1);
+		//cout << line1 <<" "<<line2<<" "<<line3 << endl;
+		int x=atoi(s1.c_str());
+        //cout <<"s1 je"<<s1<<" ,a X je "<<x <<endl;
+        line3.erase(0,2);
+        //cout << "Line3 nakon brisanja je"<<line3 << endl;
+		int y=atoi(line3.c_str());
+        ///cout <<"Y je "<<y <<endl;		
+        double uk=(x*12 + y)*2.54;
+		printf("%lf\n",uk);
+		par.uk=a;
+		par.koef=b/uk;
+		
 		polje[brojac]=par;
 		++brojac; 
    	}
 
 	myfile.close();
-set <string>::iterator si;
-printf("U setu se nalazi:\n");
-/*double rezultat;
-int imam_ukupno=0;
 ofstream novifile;
-cout << "Duljina set je" << s.size() << endl;
-novifile.open("dr1.csv");
-cout << "poslije kreirananja" << endl;
-novifile << "drzava" <<","<< "overall"<<","<<"koef"<<endl;
-printf("Tu sam ;I - prije for petlje\n");
-for(si=s.begin();si!=s.end();++si)
-{
-	cout << mapa1[*si]<<endl;
- 	if(mapa1[*si]>23) {
-				for(int i =0;i<brojac;++i)
-				{
-					if(polje[i].ime==*si) v.push_back(polje[i].broj);
-				}			
-			  
-				sort(v.begin(),v.end());
-				reverse(v.begin(),v.end());
-				for(int j=0;j<=24;++j)
-				{
-					mapa2[*si]+=v[j];
-				}
-				v.clear();
-			//	cout << *si <<" "<<mapa1[*si]<<" " <<mapa2[*si];
-				
-				rezultat=mapa2[*si]/24;
-				novifile << *si<< ","<< rezultat  << endl;	
-			}
-}
-			 
-
-//printf("Duljina seta je: %d\n",s.size());
-//printf("Na kraju imam ukupno %d država\n",imam_ukupno);
-*/
+novifile.open("koef.csv");
+novifile << "overall" <<","<< "koef"<<endl;
+    for(int i=0;i<brojac;++i)
+    {
+        novifile << polje[i].uk <<","<<polje[i].koef<<endl;    
+    }
 return 0;
-
 }
 
